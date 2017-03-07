97a15fec6f1be9062f5307eae7a03a140d31c536
Peter Han <phan@salesforce.com>
2017-03-07 03:07:30 -0800

    Send a reply back to the user

--- web.js
+++ web.js
@@ -4,6 +4,7 @@
     var express = require('express');
     var bodyParser = require('body-parser');
     var _ = require('lodash');
+    var request = require('request-promise');
 
     var app = express();
     var PORT = process.env.PORT || 9001;
@@ -22,6 +23,35 @@
     app.post('/', function (req, res) {
         console.log(req.get('X-Line-Signature'));
         console.log(JSON.stringify(req.body));
+
+        _.forEach(req.body.events, function (event) {
+            if (event.type !== 'message') {
+                return;
+            }
+
+            message = event.message;
+
+            if (message.type !== 'text') {
+                return;
+            }
+
+            request({
+                method: 'POST',
+                uri: 'https://api.line.me/v2/bot/message/reply',
+                headers: {
+                    Authorization: 'Bearer ' + process.env.LINE_CHANNEL_ACCESS_TOKEN,
+                },
+                body: {
+                    replyToken: event.replyToken,
+                    messages: [{
+                        type: 'text',
+                        text: message.text
+                    }]
+                },
+                json: true
+            })
+        });
+
         res.json(req.body);
     });
 })();
