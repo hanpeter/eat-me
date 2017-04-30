57b298cdcaef1ec44e517a2c568e089e9740837b
Peter Han <phan@salesforce.com>
2017-04-29 22:38:38 -0700

    X-Line-Signature handling

--- web.js
+++ web.js
@@ -14,54 +14,61 @@
     var app = express();
     var PORT = process.env.PORT || 9001;
 
-    // app.use(bodyParser.json());
-    // app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
     app.listen(PORT, function () {
         console.log('Server started on port ' + PORT);
     });
 
     app.post('/', function (req, res) {
-        console.log(req.body);
-        console.log(req.get('X-Line-Signature'));
-        console.log(JSON.stringify(req.body));
+        console.log('Received request: ' + JSON.stringify(req.body));
 
-        // var replies = [];
-        //
-        // _.forEach(req.body.events, function (event) {
-        //     replies.push(
-        //         line.getMessage(event)
-        //             .then(function (message) {
-        //                 return Promise.try(function () {
-        //                     return message.split('yelp.com/biz/')[1];
-        //                 });
-        //             })
-        //             .then(function (businessId) {
-        //                 return yelp.getBusiness(businessId);
-        //             })
-        //             .then(function (business) {
-        //                 var msgText = [
-        //                     business.name,
-        //                     'has rating of',
-        //                     business.rating,
-        //                     'and is currently',
-        //                     (business.hours[0].is_open_now ? '' : 'not'),
-        //                     'open'
-        //                 ].join(' ');
-        //
-        //                 return line.reply(event.replyToken, msgText);
-        //             })
-        //             .catch(errors.InvalidEventError, function (error) {
-        //                 return;
-        //             })
-        //             .catch(errors.InvalidMessageError, function (error) {
-        //                 return line.reply(event.replyToken, strings.GenericUsageMessage);
-        //             })
-        //     )
-        // });
-        //
-        // Promise.all(replies).then(function () {
-        //     res.json(req.body);
-        // });
+        if (!line.checkSignature(req.get('X-Line-Signature'), req.body)) {
+            res.sendStatus(403);
+        }
+
+        var replies = [];
+
+        _.forEach(req.body.events, function (event) {
+            replies.push(
+                line.getMessage(event)
+                    .then(function (message) {
+                        return Promise.try(function () {
+                            if (message.indexOf('yelp.com/biz/') < 0) {
+                                console.log('Unable to find yelp link');
+                                throw new errors.InvalidMessageError();
+                            }
+
+                            return message.split('yelp.com/biz/')[1];
+                        });
+                    })
+                    .then(function (businessId) {
+                        return yelp.getBusiness(businessId);
+                    })
+                    .then(function (business) {
+                        var msgText = [
+                            business.name,
+                            'has rating of',
+                            business.rating,
+                            'and is currently',
+                            (business.hours[0].is_open_now ? '' : 'not'),
+                            'open'
+                        ].join(' ');
+
+                        return line.reply(event.replyToken, msgText);
+                    })
+                    .catch(errors.InvalidEventError, function (error) {
+                        return;
+                    })
+                    .catch(errors.InvalidMessageError, function (error) {
+                        return line.reply(event.replyToken, strings.GenericUsageMessage);
+                    })
+            )
+        });
+
+        Promise.all(replies).then(function () {
+            res.sendStatus(200);
+        });
     });
 })();
