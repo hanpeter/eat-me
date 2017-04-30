3a89aff0bb6fe2facdfd5528766d50cb0b7b038e
Peter Han <phan@salesforce.com>
2017-04-29 22:16:12 -0700

    Testing commit 2

--- web.js
+++ web.js
@@ -14,8 +14,8 @@
     var app = express();
     var PORT = process.env.PORT || 9001;
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
+    // app.use(bodyParser.json());
+    // app.use(bodyParser.urlencoded({ extended: true }));
 
     app.listen(PORT, function () {
         console.log('Server started on port ' + PORT);
@@ -26,42 +26,42 @@
         console.log(req.get('X-Line-Signature'));
         console.log(JSON.stringify(req.body));
 
-        var replies = [];
-
-        _.forEach(req.body.events, function (event) {
-            replies.push(
-                line.getMessage(event)
-                    .then(function (message) {
-                        return Promise.try(function () {
-                            return message.split('yelp.com/biz/')[1];
-                        });
-                    })
-                    .then(function (businessId) {
-                        return yelp.getBusiness(businessId);
-                    })
-                    .then(function (business) {
-                        var msgText = [
-                            business.name,
-                            'has rating of',
-                            business.rating,
-                            'and is currently',
-                            (business.hours[0].is_open_now ? '' : 'not'),
-                            'open'
-                        ].join(' ');
-
-                        return line.reply(event.replyToken, msgText);
-                    })
-                    .catch(errors.InvalidEventError, function (error) {
-                        return;
-                    })
-                    .catch(errors.InvalidMessageError, function (error) {
-                        return line.reply(event.replyToken, strings.GenericUsageMessage);
-                    })
-            )
-        });
-
-        Promise.all(replies).then(function () {
-            res.json(req.body);
-        });
+        // var replies = [];
+        //
+        // _.forEach(req.body.events, function (event) {
+        //     replies.push(
+        //         line.getMessage(event)
+        //             .then(function (message) {
+        //                 return Promise.try(function () {
+        //                     return message.split('yelp.com/biz/')[1];
+        //                 });
+        //             })
+        //             .then(function (businessId) {
+        //                 return yelp.getBusiness(businessId);
+        //             })
+        //             .then(function (business) {
+        //                 var msgText = [
+        //                     business.name,
+        //                     'has rating of',
+        //                     business.rating,
+        //                     'and is currently',
+        //                     (business.hours[0].is_open_now ? '' : 'not'),
+        //                     'open'
+        //                 ].join(' ');
+        //
+        //                 return line.reply(event.replyToken, msgText);
+        //             })
+        //             .catch(errors.InvalidEventError, function (error) {
+        //                 return;
+        //             })
+        //             .catch(errors.InvalidMessageError, function (error) {
+        //                 return line.reply(event.replyToken, strings.GenericUsageMessage);
+        //             })
+        //     )
+        // });
+        //
+        // Promise.all(replies).then(function () {
+        //     res.json(req.body);
+        // });
     });
 })();
